51ad868854ff362ea32fb305b9d0dda4a8b58ab4
Seunghoon Ko <imskojs@gmail.com>
2015-06-09 20:20:59 +0900

    fix to sync

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var fs = require('fs-extra');
 var wrench = require('wrench');
 var gm = require('gm')
 
-fs.copy('from', 'to', function (err){
+fs.copySync('from', 'to', function (err){
   if (err) console.log(err);
 })
 
@@ -18,8 +18,8 @@ var files = wrench.readdirSyncRecursive('to');
 files.forEach(function(file, i, self){
   gm('to/' + file).identify(function(err, value){
     if(value.format === 'JPEG' || value.format === 'GIF' || value.format === 'PNG'){
-      if(value.size.width > 400){
-        gm('to/' + file).resize(400).write('to/' + file, function (err){
+      if(value.size.width > 415){
+        gm('to/' + file).resize(415).write('to/' + file, function (err){
           if(!err) console.log('to/'+ file + ' 변환성공!')
         })
       }
